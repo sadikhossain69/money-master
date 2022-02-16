1048d836175f0d5e7760c6dbeaeef6d37580d89f
sadikhossain69 <sadikhossain433@gmail.com>
2022-02-17 05:26:46 +0600

    funtion an update

--- js/app.js
+++ js/app.js
@@ -1,29 +1,23 @@
-// variable declare
-// const incomeInput = document.getElementById("income-input");
-// const foodInput = document.getElementById("food-input");
-// const rentInput = document.getElementById("rent-input");
-// const clothesInput = document.getElementById("clothes-input");
-// const totalExpenses = document.getElementById("total-expenses");
-// const balance = document.getElementById('balance')
 
-function isInput (event1, event2) {
-   const input = document.getElementById(event1 + event2)
-   return input
+function isUpdate(event1, event2) {
+  const allID = document.getElementById(event1 + event2);
+  return allID;
 }
 
 // calculate button handle
-document.getElementById('calculator-btn').addEventListener('click', function(){
-
+document
+  .getElementById("calculator-btn")
+  .addEventListener("click", function () {
     // total expenses handle
-    const foodValue = isInput('food', '-input').value
-    const rentValue = isInput('rent', '-input').value
-    const clothesValue = isInput('clothes', '-input').value
-    const totalExpensesAmount = parseFloat(foodValue) + parseFloat(rentValue) + parseFloat(clothesValue);
-    const totalExpenses = isInput('expenses', '-total')
-    totalExpenses.innerText = totalExpensesAmount
-    const incomeValue = isInput('income', '-input').value
-    const totalBalance = parseFloat(incomeValue) - totalExpensesAmount
-    const balance = isInput('balance', '-total')
-    balance.innerText = totalBalance
-
-})
+    const foodValue = isUpdate("food", "-input").value;
+    const rentValue = isUpdate("rent", "-input").value;
+    const clothesValue = isUpdate("clothes", "-input").value;
+    const totalExpensesAmount =
+      parseFloat(foodValue) + parseFloat(rentValue) + parseFloat(clothesValue);
+    const totalExpenses = isUpdate("expenses", "-total");
+    totalExpenses.innerText = totalExpensesAmount;
+    const incomeValue = isUpdate("income", "-input").value;
+    const totalBalance = parseFloat(incomeValue) - totalExpensesAmount;
+    const balance = isUpdate("balance", "-total");
+    balance.innerText = totalBalance;
+  });
