1f463fe96b028ec2f5f03047796de61353a72b26
sadikhossain69 <sadikhossain433@gmail.com>
2022-02-17 05:13:53 +0600

    all parts are update

--- js/app.js
+++ js/app.js
@@ -1,22 +1,29 @@
 // variable declare
-const incomeInput = document.getElementById("income-input");
-const foodInput = document.getElementById("food-input");
-const rentInput = document.getElementById("rent-input");
-const clothesInput = document.getElementById("clothes-input");
-const totalExpenses = document.getElementById("total-expenses");
-const balance = document.getElementById('balance')
+// const incomeInput = document.getElementById("income-input");
+// const foodInput = document.getElementById("food-input");
+// const rentInput = document.getElementById("rent-input");
+// const clothesInput = document.getElementById("clothes-input");
+// const totalExpenses = document.getElementById("total-expenses");
+// const balance = document.getElementById('balance')
+
+function isInput (event1, event2) {
+   const input = document.getElementById(event1 + event2)
+   return input
+}
 
 // calculate button handle
 document.getElementById('calculator-btn').addEventListener('click', function(){
 
     // total expenses handle
-    const foodValue = foodInput.value
-    const rentValue = rentInput.value
-    const clothesValue = clothesInput.value
+    const foodValue = isInput('food', '-input').value
+    const rentValue = isInput('rent', '-input').value
+    const clothesValue = isInput('clothes', '-input').value
     const totalExpensesAmount = parseFloat(foodValue) + parseFloat(rentValue) + parseFloat(clothesValue);
+    const totalExpenses = isInput('expenses', '-total')
     totalExpenses.innerText = totalExpensesAmount
-    const incomeValue = incomeInput.value
+    const incomeValue = isInput('income', '-input').value
     const totalBalance = parseFloat(incomeValue) - totalExpensesAmount
+    const balance = isInput('balance', '-total')
     balance.innerText = totalBalance
 
 })
