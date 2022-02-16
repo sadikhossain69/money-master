c1e07f38366c359c036294c5036aade9bd13a8e0
sadikhossain69 <sadikhossain433@gmail.com>
2022-02-17 01:45:56 +0600

    comment deleted

--- js/app.js
+++ js/app.js
@@ -6,11 +6,6 @@ const clothesInput = document.getElementById("clothes-input");
 const totalExpenses = document.getElementById("total-expenses");
 const balance = document.getElementById('balance')
 
-// function getUserInput () {
-//   const input = document.getElementById("income-input");
-//   return input
-// }
-
 // calculate button handle
 document.getElementById('calculator-btn').addEventListener('click', function(){
 
